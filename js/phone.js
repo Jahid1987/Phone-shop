0b50369d984db3fab9ec8daa29ac5ac8de40788b
Jahid Anwer <jahid.englaa@gmail.com>
2024-02-28 01:44:42 +0600

    show all button, loading spinner functionality added

--- js/phone.js
+++ js/phone.js
@@ -1,18 +1,27 @@
 // fetching data from open api
-async function loadPhones(querry) {
+async function loadPhones(querry, isshowAll) {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${querry}`
   );
   const data = await response.json();
   const phones = data.data;
-  displayPhones(phones);
+  displayPhones(phones, isshowAll);
 }
 
 // showing data on ui
-function displayPhones(phones) {
+function displayPhones(phones, isshowAll) {
   const phoneContainer = document.getElementById("phone-container");
   // Clearing container before loading items
   phoneContainer.textContent = "";
+
+  //   Toggle show all button if data is more than 12 and isshowAll is not
+  const showAllButton = document.getElementById("show-all-btn");
+  phones.length > 12 && !isshowAll
+    ? showAllButton.classList.remove("hidden")
+    : showAllButton.classList.add("hidden");
+
+  //   showing only 12 phones if data is more than 12 and isshowAll is not
+  phones = !isshowAll ? phones.slice(0, 12) : phones;
 
   phones.forEach((phone) => {
     // creating div for card
@@ -33,10 +42,27 @@ function displayPhones(phones) {
     // appending child to phone container
     phoneContainer.appendChild(card);
   });
+
+  toggleLoading(false);
 }
 
 // search handle
-const handleSearch = async () => {
-  const searchText = document.getElementById("search-text").value;
-  loadPhones(searchText);
+const handleSearch = (isshowAll) => {
+  toggleLoading(true);
+  const searchText = document.getElementById("search-text");
+  const querry = searchText.value;
+  loadPhones(querry, isshowAll);
+};
+
+// show all button
+const showAll = () => {
+  handleSearch(true);
+};
+
+// toggling loader
+const toggleLoading = (isLoading) => {
+  const showLoader = document.getElementById("show-loader");
+  isLoading
+    ? showLoader.classList.remove("hidden")
+    : showLoader.classList.add("hidden");
 };
