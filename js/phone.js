d37cb25ef973a096d77b6464dd6cd726f6f661cb
Jahid Anwer <jahid.englaa@gmail.com>
2024-02-27 23:07:46 +0600

    basic search functionality added

--- js/phone.js
+++ js/phone.js
@@ -1,18 +1,42 @@
 // fetching data from open api
-async function loadPhones() {
+async function loadPhones(querry) {
   const response = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${querry}`
   );
   const data = await response.json();
   const phones = data.data;
-  showPhones(phones);
-}
-// showing data on ui 
-function showPhones(phones) {
-    // console.log(phones)
-    phones.forEach(element => {
-        console.log(element.phone_name)
-    });
+  displayPhones(phones);
 }
 
+// showing data on ui
+function displayPhones(phones) {
+  const phoneContainer = document.getElementById("phone-container");
+  // Clearing container before loading items
+  phoneContainer.textContent = "";
 
+  phones.forEach((phone) => {
+    // creating div for card
+    const card = document.createElement("div");
+    // adding class
+    card.classList = `card p-4 bg-base-100 shadow-xl`;
+    // adding inner html
+    card.innerHTML = `
+        <figure><img src="${phone.image}" alt="${phone.slug}" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>${phone.brand}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+        `;
+    // appending child to phone container
+    phoneContainer.appendChild(card);
+  });
+}
+
+// search handle
+const handleSearch = async () => {
+  const searchText = document.getElementById("search-text").value;
+  loadPhones(searchText);
+};
